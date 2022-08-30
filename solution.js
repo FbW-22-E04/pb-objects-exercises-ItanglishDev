559577df3a52523fb640ea6eff631e2028491b44
chris <chrishethertonweb@gmail.com>
2022-08-30 13:59:22 +0200

    first exercise done

--- solution.js
+++ solution.js
@@ -1,10 +1,26 @@
 //1
 const data = { a: 1 };
 //write your code here 
+function isPlainObject(item) {
+
+  if (typeof item === 'object' && item !== null && !Array.isArray(item)) {
+    return true
+  }
+}
+
 console.log(isPlainObject(data)); // true
 
 //2
 const data2 = { a: 1, b: 2 };
+
+function makePairs(obj) {
+
+  const array = []
+
+  for (const key in object) {
+    array.push(key.object)
+  }
+}
 //write your code here 
 console.log(makePairs(data2)); // [['a', 1], ['b', 2]] 
 
@@ -16,14 +32,14 @@ console.log(without(data3, 'b')); // { a: 1 }
 
 //4
 const data4 = { a: 1, b: undefined };
-const data40 = { a:undefined };
+const data40 = { a: undefined };
 //write your code here 
 console.log(isEmpty(data4)); // false
 console.log(isEmpty(data40)); // true 
 
 //5
-const data5 = { a: 1, b: 1 };  
-const data51 = { a: 1, b: 1 };  
+const data5 = { a: 1, b: 1 };
+const data51 = { a: 1, b: 1 };
 const data52 = { a: 1, b: 2 };
 //write your code here 
 console.log(isEqual(data5, data51)); // true  
@@ -42,23 +58,23 @@ console.log(isEmptyDeep(data7));
 
 
 //8
-const data8 = { a: 1, b: { c: 1 } };  
-const data81= { a: 1, b: { c: 1 } };  
-const data82= { a: 1, b: { c: 2 } };
+const data8 = { a: 1, b: { c: 1 } };
+const data81 = { a: 1, b: { c: 1 } };
+const data82 = { a: 1, b: { c: 2 } };
 //write your code here 
 console.log(isEqualDeep(data8, data81));// true  
 console.log(isEqualDeep(data8, data82)); // false  
 
 
 //9
-const data9 = { a: 1, b: 2 };  
+const data9 = { a: 1, b: 2 };
 const data91 = { c: 1, b: 2 };
 //write your code here 
 console.log(intersection(data9, data91)); // { b: 2 }
 
 
 //10
-const data10 = { a: 1, b: { c: 3 } };  
+const data10 = { a: 1, b: { c: 3 } };
 const data11 = { c: 1, b: { c: 3 } };
 //write your code here 
 console.log(intersectionDeep(data10, data11)); // { b: { c: 3 } }
